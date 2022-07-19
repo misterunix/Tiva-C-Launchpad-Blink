c44b539109e45883d73acfbfb74e62acfac1f64a
Bill Jones <misterunix@gmail.com>
2022-07-19 16:31:35 -0500

    added serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,56 @@
 #include <Arduino.h>
 
+int incomingByte = 0; // for incoming serial data
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(PF_1, OUTPUT); // Red LED
   pinMode(PF_2, OUTPUT); // Blue LED
   pinMode(PF_3, OUTPUT); // Green LED
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    if (incomingByte == '1') {
+      Serial.print("red\n");
+      digitalWrite(PF_1, HIGH);
+      digitalWrite(PF_2, LOW);
+      digitalWrite(PF_3, LOW);
+    }
+    if (incomingByte == '2') {
+      Serial.print("blue\n");
+      digitalWrite(PF_1, LOW);
+      digitalWrite(PF_2, HIGH);
+      digitalWrite(PF_3, LOW);
+    }
+    if (incomingByte == '3') {
+      Serial.print("green\n");
+      digitalWrite(PF_1, LOW);
+      digitalWrite(PF_2, LOW);
+      digitalWrite(PF_3, HIGH);
+    }
+    /*
+  Serial.print("red\n");
   digitalWrite(PF_1, HIGH);
-  delay(100);
+  delay(1000);
   digitalWrite(PF_1, LOW);
-  delay(100);
+  delay(1000);
+  Serial.print("blue\n");
   digitalWrite(PF_2, HIGH);
-  delay(100);
+  delay(1000);
   digitalWrite(PF_2, LOW);
-  delay(100);
+  delay(1000);
+  Serial.print("green\n");
   digitalWrite(PF_3, HIGH);
-  delay(100);
+  delay(1000);
   digitalWrite(PF_3, LOW);
-  delay(100);
+  delay(1000);
+*/
+  }
 }
