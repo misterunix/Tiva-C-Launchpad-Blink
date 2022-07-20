6f894d314b40828f59fcd459bc524fe3253360ee
Bill Jones <misterunix@gmail.com>
2022-07-19 20:01:14 -0500

    int and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# TI Tiva-C Launchpad
+
+Its been YEARS since I have messed with any of the mmicrocontrollers I own.
+
+Since I am in a Bipolar funk, I am destracting myself with them.
+
+Dont expect anything earthshaking here.
+
+Bill
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,61 @@
 #include <Arduino.h>
 
 int incomingByte = 0; // for incoming serial data
+volatile byte state = LOW;
+const byte interruptPin = PUSH1;
 
-void setup() {
+void blink()
+{
+  state = !state;
+}
+
+void setup()
+{
   // put your setup code here, to run once:
   pinMode(PF_1, OUTPUT); // Red LED
   pinMode(PF_2, OUTPUT); // Blue LED
   pinMode(PF_3, OUTPUT); // Green LED
+
+  pinMode(interruptPin, INPUT_PULLUP);
+
+  attachInterrupt(interruptPin, blink, CHANGE);
+
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
-  if (Serial.available() > 0) {
+  digitalWrite(PF_1, state);
+
+  if (Serial.available() > 0)
+  {
     // read the incoming byte:
     incomingByte = Serial.read();
 
-    if (incomingByte == '1') {
+    if (incomingByte == '1')
+    {
       Serial.print("red\n");
       digitalWrite(PF_1, HIGH);
       digitalWrite(PF_2, LOW);
       digitalWrite(PF_3, LOW);
     }
-    if (incomingByte == '2') {
+    if (incomingByte == '2')
+    {
       Serial.print("blue\n");
       digitalWrite(PF_1, LOW);
       digitalWrite(PF_2, HIGH);
       digitalWrite(PF_3, LOW);
     }
-    if (incomingByte == '3') {
+    if (incomingByte == '3')
+    {
       Serial.print("green\n");
       digitalWrite(PF_1, LOW);
       digitalWrite(PF_2, LOW);
       digitalWrite(PF_3, HIGH);
     }
+
     /*
   Serial.print("red\n");
   digitalWrite(PF_1, HIGH);
